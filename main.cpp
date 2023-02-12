7b2005db74c2a12d3465ba99c1757d6836af718b
llleiron <60439499+llleiron@users.noreply.github.com>
2023-02-13 00:18:40 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,21 @@
 //    return distr(gen);
 // }
 #include "cards.h"
+#include "deck.h"
 #include <iostream>
 
 
 int main(){
     
-    Card card(Suits::CLUBS, CardNames::TWO);
-    card.print_card();
+    Card card(Suits::DIAMONDS, CardNames::FOUR);
+    Card card1(Suits::DIAMONDS, CardNames::TWO);
+    Deck my_deck;
+
+    my_deck.delete_card(card);
+    std::cout << my_deck.find_card(card);
+    std::vector<Card> current_deck = my_deck.get_deck();
+    
+    // for(auto el: current_deck){
+    //     el.print_card();
+    // }
 }

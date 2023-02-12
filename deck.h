01960201734b4c0393f6cc0900b1b5f8e4e1e6cd
llleiron <60439499+llleiron@users.noreply.github.com>
2023-02-13 00:17:59 +0400

    Update deck.h

--- deck.h
+++ deck.h
@@ -1,1 +1,20 @@
-#include ca
+#include "cards.h"
+#include <vector>
+
+#ifndef DECK_H
+#define DECK_H
+
+class Deck{
+    private:
+        std::vector<Card> _deck;
+    
+    public:
+        Deck();
+        Deck(const Deck& deck);
+        void delete_card(Card& card);
+        int find_card(const Card& card) const;
+        Card get_random_card();
+        std::vector<Card> get_deck() const;
+};
+
+#endif

2bf4d01daa07cd97aeda5125825c9d6c839ff0d7
llleiron <60439499+llleiron@users.noreply.github.com>
2023-02-13 10:37:19 +0400

    Update deck_impl.cpp

--- deck_impl.cpp
+++ deck_impl.cpp
@@ -1,7 +1,18 @@
 #include "deck.h"
+#include "utility_funcs.h"
 #include <algorithm>
 
+
 Deck::Deck(){
+    for(int i = (int)Suits::CLUBS; i <= (int)Suits::SPADES; ++i){
+        for(int j = (int)CardNames::ACE; j <= (int)CardNames::KING; ++j){
+            Card current_card(static_cast<Suits>(i), static_cast<CardNames>(j));
+            _deck.push_back(current_card);
+        }
+    }
+}
+
+void Deck::setup(){
     for(int i = (int)Suits::CLUBS; i <= (int)Suits::SPADES; ++i){
         for(int j = (int)CardNames::ACE; j <= (int)CardNames::KING; ++j){
             Card current_card(static_cast<Suits>(i), static_cast<CardNames>(j));
@@ -27,4 +38,23 @@ int Deck::find_card(const Card& card) const{
 
 std::vector<Card> Deck::get_deck() const{
     return _deck;
+}
+
+Card Deck::get_random_card(){
+    if(_deck.size() == 0){
+        throw();
+    }
+    Suits suit = static_cast<Suits>(generate_random_number(1, 4));
+    CardNames name = static_cast<CardNames>(generate_random_number(1, 13));
+
+    Card cur_card(suit, name);
+    
+    while(find_card(cur_card) == -1){
+        suit = static_cast<Suits>(generate_random_number(1, 4));
+        name = static_cast<CardNames>(generate_random_number(1, 13));
+        cur_card.set_name(name);
+        cur_card.set_suit(suit);
+    }
+
+    return cur_card;
 }

949bee783c964a0fc4a52aa1e956cf3b2f65ac03
llleiron <60439499+llleiron@users.noreply.github.com>
2023-02-21 18:09:31 +0400

    Delete utility_funcs.cpp

--- utility_funcs.cpp
+++
@@ -1,9 +0,0 @@
-#include <random>
-#include "utility_funcs.h"
-
-int generate_random_number(int lhs, int rhs){
-   std::random_device rd;
-   std::mt19937 gen(rd());
-   std::uniform_int_distribution<> distr(lhs, rhs);
-   return distr(gen);
-}
